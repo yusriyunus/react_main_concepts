ede5563ed0b09c1c4152bb138ef0f6daaee1f4b1
yunus_ <yusriyunus97.yy@gmail.com>
2019-05-28 10:34:54 +0700

    implement react-main-concepts point 10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const scaleNames = {
+  c: "Celcius",
+  f: "Fahrenheit"
+};
+
+function toCelcius(fahrenheit) {
+  return (fahrenheit - 32) * (5 / 9);
+}
+
+function toFahrenheit(celcius) {
+  return (celcius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+function BoilingVerdict(props) {
+  if (props.celcius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = e => {
+    this.props.onTemperatureChange(e.target.value);
+  };
+
+  render() {
+    const { temperature, scale } = this.props;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]} :</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      temperature: "",
+      scale: "c"
+    };
+  }
+
+  handleCelsiusChange = temperature => {
+    this.setState({ scale: "c", temperature });
+  };
+
+  handleFahrenheitChange = temperature => {
+    this.setState({ scale: "f", temperature });
+  };
+
+  render() {
+    const { scale, temperature } = this.state;
+    const celcius =
+      scale === "f" ? tryConvert(temperature, toCelcius) : temperature;
+    const fahrenheit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <div>
+        <TemperatureInput
+          scale="c"
+          temperature={celcius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celcius={parseFloat(celcius)} />
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
